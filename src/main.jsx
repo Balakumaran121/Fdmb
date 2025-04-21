ebb333ca65e30594ddd02a8b007bea79d65e212f
Balakumaran <balakumaran@cloudrevelinnovation.com>
2025-04-21 14:20:34 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react'
-
+console.log("ffffff")
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/Fdmb">
